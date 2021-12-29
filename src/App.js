a8dd306ddc56e0acfe192f7b5d15d59882036724
Ejiro Akpoduado <eau01@ukc02ql98mgg7j.home>
2021-12-29 22:21:27 +0000

    first commit

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import React from 'react' 
+
+export default function App() {
+  return (
+   <div>
+     <img width='400px' src='https://images.unsplash.com/photo-1617122287896-8638ac5c5414?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8dmFsZW5jaWF8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60' />
+   </div>
+  )
+}
